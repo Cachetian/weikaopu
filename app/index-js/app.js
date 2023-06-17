ece9030b9127e7bbb104dabc33a7dc45632cf569
Sky Mu <1989mutian@163.com>
2023-06-17 17:38:19 +0800

    Enhance index modules

--- app/index-js/app.js
+++ app/index-js/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-    res.sendFile(require("path").join(__dirname, "/index.html"));
+    res.sendFile(require("path").join(__dirname, "/index.page.html"));
 });
 
 app.listen(port, () => {
